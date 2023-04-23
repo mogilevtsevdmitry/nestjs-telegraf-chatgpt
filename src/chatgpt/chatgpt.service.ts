e77aa50f0ad13c5e1e979ff8305b13347fe37c56
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-04-24 00:09:02 +0500

    rm logger

--- src/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt.service.ts
@@ -45,7 +45,7 @@ export class ChatgptService {
             temperature: 1,
         };
         return this.httpService.post<ChatGptAnswer>(this.apiUrl, data, { headers }).pipe(
-            tap(({ data }) => this.logger.log(data)),
+            // tap(({ data }) => this.logger.log(data)),
             map((data) => data.data?.choices[0].message.content.trim()),
             catchError((err) => {
                 this.logger.error(err);
