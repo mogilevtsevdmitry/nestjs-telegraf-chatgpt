dc08ea25c2b8f2be84fe92545ca1a8d68cb828df
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-04-24 00:09:52 +0500

    rm telegraf

--- src/telegraf/telegraf.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TelegrafService } from './telegraf.service';
-import { ChatGPTModule } from '@/chatgpt/chatgpt.module';
-
-@Module({
-    imports: [ChatGPTModule],
-    providers: [TelegrafService],
-})
-export class TelegrafModule {}
